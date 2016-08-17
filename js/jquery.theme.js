ca5f0ba5d625eb85626554a71e0246d6a634ef38
Alexander Agnarson <alexander.agnarson@gmail.com>
2016-08-17 14:35:14 +0200

    Remove unused javascript

--- js/jquery.theme.js
+++ js/jquery.theme.js
@@ -50,9 +50,6 @@ jQuery(document).ready(function($) {
 /*---------------------------------------------------------------------------*
  * Scrollspy Animated
 /*---------------------------------------------------------------------------*/		
-	
-	// Add scrollspy to <body>
-	$('body').scrollspy({target: ".nav", offset: 50});   
 
 	// Add smooth scrolling on all links inside the navbar
 	$(".nav a").on('click', function(event) {
@@ -75,55 +72,5 @@ jQuery(document).ready(function($) {
 			});
 		}  // End if
 	});
-	
-	
-/*---------------------------------------------------------------------------*
- * Smooth scroll link to anchor
-/*---------------------------------------------------------------------------	
-	function filterPath(string) {
-	return string
-	.replace(/^\//,'')
-	.replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-	.replace(/\/$/,'');
-	}
-	var locationPath = filterPath(location.pathname);
-	var scrollElem = scrollableElement('html', 'body');
 
-	$('a[href*=#]').each(function() {
-	var thisPath = filterPath(this.pathname) || locationPath;
-	if (  locationPath == thisPath
-	&& (location.hostname == this.hostname || !this.hostname)
-	&& this.hash.replace(/#/,'') ) {
-	  var $target = $(this.hash), target = this.hash;
-	  if (target) {
-		var targetOffset = $target.offset().top;
-		$(this).click(function(event) {
-		  event.preventDefault();
-		  $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-			location.hash = target;
-		  });
-		});
-	  }
-	}
-	});
-
-	// use the first element that is "scrollable"
-	function scrollableElement(els) {
-	for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-	  var el = arguments[i],
-		  $scrollElement = $(el);
-	  if ($scrollElement.scrollTop()> 0) {
-		return el;
-	  } else {
-		$scrollElement.scrollTop(1);
-		var isScrollable = $scrollElement.scrollTop()> 0;
-		$scrollElement.scrollTop(0);
-		if (isScrollable) {
-		  return el;
-		}
-	  }
-	}
-	return [];
-	}
-	*/
 });
